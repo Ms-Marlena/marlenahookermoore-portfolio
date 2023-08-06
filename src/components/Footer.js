2a032caccfadfe240e465e314438c70e873c5aa4
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-08-06 11:51:03 -0400

    fixed social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,35 @@ import React from "react";
 const Footer = () => (
   <>
     <footer className="page-footer font-small blue pt-4">
-      <div className="container-fluid text-center text-md-left">
+      <div className="container-fluid text-center text-md-left bg-body-tertiary">
         <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-          </div>
+          <div className="col-md-6 mt-md-0 mt-3"></div>
           <div className="text-center py-3">
             <p>Find me on:</p>
             <p>
-              <a target="_blank" href="https://google.com">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://https://github.com/Ms-Marlena"
+              >
                 Github
               </a>
             </p>
             <p>
-              <a target="_blank" href="https://google.com">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://https://www.linkedin.com/in/marlena-moore-568b138/"
+              >
                 LinkedIn
               </a>
             </p>
             <p>
-              <a target="_blank" href="https://google.com">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/marlenahookermoore/"
+              >
                 Instagram
               </a>
             </p>
@@ -32,7 +43,10 @@ const Footer = () => (
         </div>
       </div>
 
-      <div className="footer-copyright text-center py-3">
+      <div
+        className="footer-copyright text-center py-3"
+        style={{ fontSize: "10px" }}
+      >
         © 2020 Footer Copyright:
         <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
       </div>
