186646b14491e173aceb93190ab99df23e7f7f64
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-10-06 14:56:06 -0600

    fixed contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,36 @@
 import Form from 'react-bootstrap/Form';
 import Footer from './Footer';
 import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/Button";
 
 function Contact() {
   return (
     <>
       <Container>
-        <Form>
+        <Form action="https://api.web3forms.com/submit" method="POST">
+          <Form.Control
+            type="hidden"
+            name="access_key"
+            value="12db8a49-36c0-4aaa-8837-1c37c62646e6"
+          />
           <Form.Group className="mb-3" type="text" controlId="contactName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="input" placeholder="Name" />
+            <Form.Control type="input" name="name" placeholder="Name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="contactEmail">
             <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="contactMessage">
             <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" name="message" rows={3} />
           </Form.Group>
+          <Form.Control
+            type="hidden"
+            name="redirect"
+            value="https://web3forms.com/success"
+          />
+          <Button as="input" type="submit" value="Submit" />
         </Form>
       </Container>
       <Footer />
